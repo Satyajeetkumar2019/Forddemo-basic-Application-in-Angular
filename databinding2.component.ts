12251cdde686891441a6dbdc2b867df44c3827f8
satyajeet kumar <55208293+Satyajeetkumar2019@users.noreply.github.com>
2022-10-20 16:49:04 +0530

    Add files via upload
    some databinding concept Basic Applicaton now in Angular 14

---
+++ databinding2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding2',
+  templateUrl: './databinding2.component.html',
+  styleUrls: ['./databinding2.component.sass']
+})
+export class Databinding2Component implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
